f9aade6df09287eccdc9d95116107484245f1b45
Carlos A Santos <carlos.itdeveloper@gmail.com>
2023-06-03 16:04:13 -0300

    add components/big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BigCardComponent
   ],
   imports: [
     BrowserModule,
